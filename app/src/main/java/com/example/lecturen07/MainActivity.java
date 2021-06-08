9b02f1b69a61d707d9e95abccee5618ab158a249
unknown <asmak9060@gamil.com>
2021-06-08 07:28:04 +0500

    3rd commit

--- app/src/main/java/com/example/lecturen07/MainActivity.java
+++ app/src/main/java/com/example/lecturen07/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.lecturen07;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,19 +9,18 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    TextView textView;
     int Count=20;
-    TextView textView,textView2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView=findViewById(R.id.textView);
-        textView2=findViewById(R.id.textView2);
-        button=findViewById(R.id.button);
+        button=findViewById(R.id.BtnCount);
+        textView=findViewById(R.id.TextViewcounter);
     }
 
-
-    public void count(View view) {
+    public void Click(View view) {
         Count=Integer.parseInt(textView.getText().toString());
         Count--;
         textView.setText(String.valueOf(Count));
