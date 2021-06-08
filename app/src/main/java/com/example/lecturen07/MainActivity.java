0083ac5be5c2f1545e9999cb68861e5d1be98d60
unknown <asmak9060@gamil.com>
2021-06-08 07:51:19 +0500

    5th commit

--- app/src/main/java/com/example/lecturen07/MainActivity.java
+++ app/src/main/java/com/example/lecturen07/MainActivity.java
@@ -1,49 +1,39 @@
 package com.example.lecturen07;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
         Log.d("ALC","onStart called");
     }
-
     @Override
     protected void onResume() {
         super.onResume();
         Log.d("ALC","onResume called");
     }
-
     @Override
     protected void onPause() {
         super.onPause();
         Log.d("ALC","onPause called");
     }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.d("ALC","onDestroy called");
     }
-
     @Override
     protected void onStop() {
         super.onStop();
         Log.d("ALC","onStop called");
     }
-
-
     Button button;
     TextView textView;
     int Count=20;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
         textView=findViewById(R.id.TextViewcounter);
         Log.d("ALC","onCreate called");
     }
-
     public void Click(View view) {
         Count=Integer.parseInt(textView.getText().toString());
         Count--;
